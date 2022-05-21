484cb269e79f9da2020825ba8d424d7d384bd9a4
gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2022-05-21 19:30:37 -0300

    feat: components and pages boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,32 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {
+  NavBar, Footer, SiderBar, ThemeSettings,
+} from './components';
+import {
+  Ecommerce,
+  Editor,
+  Employees,
+  Calendar,
+  ColorMapping,
+  ColorPicker,
+  Customers,
+  Stacked,
+  Pie,
+  Pyramid,
+  Line,
+  Kanban,
+  Area,
+  Financial,
+  Orders,
+  Bar,
+} from './pages';
+
+import './App.css';
 
 function App() {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
@@ -18,6 +42,45 @@ function App() {
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <SiderBar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <SiderBar />
+            </div>
+          )}
+          <div
+            className={
+              `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+            }
+          >
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <NavBar />
+            </div>
+          </div>
+          <div>
+            <Routes>
+              <Route path="/" element={Ecommerce} />
+              <Route path="/ecommerce" element={Ecommerce} />
+              <Route path="/orders" element={Orders} />
+              <Route path="/employees" element={Employees} />
+              <Route path="/customers" element={Customers} />
+              <Route path="/kanban" element={Kanban} />
+              <Route path="/calendar" element={Calendar} />
+              <Route path="/editor" element={Editor} />
+              <Route path="/color-picker" element={ColorPicker} />
+              <Route path="/pie" element={Pie} />
+              <Route path="/line" element={Line} />
+              <Route path="/area" element={Area} />
+              <Route path="/bar" element={Bar} />
+              <Route path="/financial" element={Financial} />
+              <Route path="/color-mapping" element={ColorMapping} />
+              <Route path="/stacked" element={Stacked} />
+              <Route path="/pyramid" element={Pyramid} />
+            </Routes>
           </div>
         </div>
       </BrowserRouter>
---
+++ src/components/Charts/StackedChart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function StackedChart() {
+  return (
+    <div>StackedChart</div>
+  );
+}
+
+export default StackedChart;
